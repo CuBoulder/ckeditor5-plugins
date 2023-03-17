6227883addd8d606399a4f296a3f114f4cdd27d0
Pat <breepatbree@gmail.com>
2023-03-17 12:48:28 -0600

    wip: command file is needed, still no schema

--- ucb_ckeditor5_invisible/js/ckeditor5_plugins/invisible/src/invisibleediting.js
+++ ucb_ckeditor5_invisible/js/ckeditor5_plugins/invisible/src/invisibleediting.js
@@ -1,24 +1,46 @@
-// WHERE YOU SETUP SCHEMA, translates into HTML
-
 import { Plugin } from 'ckeditor5/src/core';
+import InvisibleCommand from './insertinvisiblecommand';
 
 export default class InvisibleEditing extends Plugin {
+	static get requires() {
+		return [Widget];
+	  }	
+
 	init() {
 		this._defineSchema();
 		this._defineConverters();
+		this.editor.commands.add(
+			'addInvisible',
+			new InvisibleCommand(this.editor),
+		  );
 	}
 	_defineSchema() {
 		const schema = this.editor.model.schema;
     	// Extend the text node's schema to accept the tooltip attribute.
 		schema.register( 'ucb-invisible', {
-			inheritAllFrom: '$text'
+			allowIn: '$root',
+			allowWhere: '$text',
+			allowChildren: '$text'
+		} );
+
+		schema.extend( '$text', {
+			allowAttributes: [ 'ucb-invisible' ]
 		} );
 	}
 	_defineConverters() {
-		const conversion = this.editor.conversion;
-		
+		const {conversion} = this.editor;
+
+		// Conversion from a view element to a model attribute
+		conversion.for( 'upcast' ).elementToElement( {
+			model: 'ucb-invisible',
+			view: {
+				name: 'span',
+				classes:  'sr-only'
+			},
+		} );
+
         // Conversion from a model attribute to a view element
-		conversion.for( 'downcast' ).elementToElement( {
+		conversion.for( 'downcast' ).attributeToElement( {
 			model: 'ucb-invisible',
 
             // Callback function provides access to the model attribute value
@@ -29,13 +51,6 @@ export default class InvisibleEditing extends Plugin {
 			}
 		} );
 
-		// Conversion from a view element to a model attribute
-		conversion.for( 'upcast' ).elementToElement( {
-			view: {
-				name: 'span',
-				classes:  'sr-only'
-			},
-			model: 'ucb-invisible',
-		} );
+
 	}
 }
