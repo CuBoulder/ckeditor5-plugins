40ca5e4bd8302a6bf0abd095ff563cf9837b65eb
Pat <breepatbree@gmail.com>
2023-03-21 14:52:17 -0600

    Invisible applies, needed editDowncast

--- ucb_ckeditor5_invisible/js/ckeditor5_plugins/invisible/src/insertinvisiblecommand.js
+++ ucb_ckeditor5_invisible/js/ckeditor5_plugins/invisible/src/insertinvisiblecommand.js
@@ -4,6 +4,7 @@
  */
 // cSpell:ignore simpleboxeditingimport { Command } from 'ckeditor5/src/core';
 import { Command } from 'ckeditor5/src/core';
+import { IconView } from 'ckeditor5/src/ui';
 
 
 export default class InvisibleCommand extends Command {
@@ -13,8 +14,9 @@ export default class InvisibleCommand extends Command {
     model.change(writer => {
       // Insert <ucb-invisible>*</ucb-invisible> at the current selection position
       // in a way that will result in creating a valid model structure.
-      const invisible = addInvisible(writer);
-      model.insertContent(invisible, model.document.selection);
+      const selectedText = this.editor.getData({ selection: true });
+      const invisible = addInvisible(writer, selectedText);
+      model.insertContent(invisible);
     });
   }
 
@@ -36,10 +38,15 @@ export default class InvisibleCommand extends Command {
   }
 }
 
-function addInvisible(writer) {
+function addInvisible(writer, text) {
   // Create instances of the element registered with the editor.
 
+  // Make the element
   const invisible = writer.createElement('ucb-invisible');
+  // Strip the tags out of the selected text, convert to text Node for append
+  const textNode = writer.createText(text.replace(/<\/?[^>]+(>|$)/g, ""))
+  // Append
+  writer.append(textNode, invisible)
 
   // Return the element to be added to the editor.
   return invisible;
--- ucb_ckeditor5_invisible/js/ckeditor5_plugins/invisible/src/invisibleediting.js
+++ ucb_ckeditor5_invisible/js/ckeditor5_plugins/invisible/src/invisibleediting.js
@@ -21,58 +21,41 @@ export default class InvisibleEditing extends Plugin {
     	// Extend the text node's schema to accept the tooltip attribute.
 		schema.register( 'ucb-invisible', {
 			allowWhere: '$block',
-			allowChildren: ['paragraph', 'text', 'em'] // add 'em' to allowChildren
+			allowChildren: '$text'
 		} );
 	}
 	_defineConverters() {
-		const { conversion } = this.editor;
-	  
+		const {conversion} = this.editor;
+
 		// Conversion from a view element to a model attribute
-		conversion.for('upcast').elementToElement({
-		  view: {
-			name: 'span',
-			classes: 'sr-only'
-		  },
-		  model: (viewElement, { writer: modelWriter }) => {
-			// Create a `ucb-invisible` model element with an empty text node
-			return modelWriter.createElement('ucb-invisible');
-		  }
-		});
-	  
-		// Conversion from a model element to a view element
-		conversion.for('dataDowncast').elementToElement({
-		  model: 'ucb-invisible',
-		  view: (modelElement, { writer: viewWriter }) => {
-			// Create a `span` view element with the `sr-only` class
-			const spanElement = viewWriter.createContainerElement('span', {
-			  class: 'sr-only'
-			});
-			// Iterate through the child nodes of the `ucb-invisible` model element
-			// and add them to the `span` view element
-			for (const childNode of modelElement.getChildren()) {
-			  const viewChildNode = conversion.convertModelToView(childNode);
-			  viewWriter.insert(viewChildNode, spanElement);
+		conversion.for( 'upcast' ).elementToElement( {
+			model: 'ucb-invisible',
+			view: {
+				name: 'span',
+				classes:  'sr-only'
+			},
+		} );
+
+        // Conversion from a model attribute to a view element
+		conversion.for( 'dataDowncast' ).elementToElement( {
+			model: 'ucb-invisible',
+
+            // Callback function provides access to the model attribute value
+			// and the DowncastWriter
+			view: {
+				name: 'span',
+				classes: 'sr-only',
 			}
-			return spanElement;
-		  }
-		});
-	  
-		// Conversion from a model element to a view element
+		} );
+
 		conversion.for('editingDowncast').elementToElement({
-		  model: 'ucb-invisible',
-		  view: (modelElement, { writer: viewWriter }) => {
-			// Create a `span` view element with the `sr-only` class
-			const spanElement = viewWriter.createEditableElement('span', {
-			  class: 'sr-only'
-			});
-			// Iterate through the child nodes of the `ucb-invisible` model element
-			// and add them to the `span` view element
-			for (const childNode of modelElement.getChildren()) {
-			  const viewChildNode = conversion.convertModelToView(childNode);
-			  viewWriter.insert(viewChildNode, spanElement);
+			model: 'ucb-invisible',
+			view: {
+				name: 'span',
+				class: 'ucb-invisible-edit'
 			}
-			return spanElement;
-		  }
-		});
-	  }
-	}	  
+		})
+
+
+	}
+}
