b237b7713c31fd11b95b3294559ed79e07b74c4e
Pat <breepatbree@gmail.com>
2023-03-23 14:49:42 -0600

    Changed how to target selection in Editor, more repeatable and doesn't grab content above/below

--- ucb_ckeditor5_invisible/js/ckeditor5_plugins/invisible/src/insertinvisiblecommand.js
+++ ucb_ckeditor5_invisible/js/ckeditor5_plugins/invisible/src/insertinvisiblecommand.js
@@ -7,15 +7,20 @@ import { Command } from 'ckeditor5/src/core';
 import { IconView } from 'ckeditor5/src/ui';
 
 
+// const selection = editor.model.document.selection;
+
+
 export default class InvisibleCommand extends Command {
   execute() {
     const model = this.editor.model;
+    const selection = model.document.selection;
 
     model.change(writer => {
       // Insert <ucb-invisible>*</ucb-invisible> at the current selection position
       // in a way that will result in creating a valid model structure.
-      const selectedText = this.editor.getData({ selection: true });
-      const invisible = addInvisible(writer, selectedText);
+
+      // const selectedText = this.editor.getData({ selection: true });
+      const invisible = addInvisible(writer, selection);
       model.insertContent(invisible);
     });
   }
@@ -44,9 +49,15 @@ function addInvisible(writer, text) {
   // Make the element
   const invisible = writer.createElement('ucb-invisible');
   // Strip the tags out of the selected text, convert to text Node for append
-  const textNode = writer.createText(text.replace(/<\/?[^>]+(>|$)/g, ""))
-  // Append
-  writer.append(textNode, invisible)
+  
+
+  const range = text.getFirstRange();
+
+  for (const item of range.getItems()) {
+    const textNode = writer.createText(item.data)
+    writer.append(textNode, invisible)
+
+  }  
 
   // Return the element to be added to the editor.
   return invisible;
